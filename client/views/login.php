6ecb98c0f91818b7bf6bc4bc3c6a05c9a308cbfb
sondinh12 <nguyendinhson92005@gmail.com>
2024-11-12 22:10:44 +0700

    dang lam

---
+++ client/views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Tên đăng nhập</label>
+        <input type="text" name="user_name" id="" placeholder="Nhập tên tài khoản">
+        <label for="">Mật khẩu</label>
+        <input type="password" name="pass" id="" placeholder="Nhập mật khẩu">
+    </form>
+</body>
+</html>
+
+<?php
+    
+?>
