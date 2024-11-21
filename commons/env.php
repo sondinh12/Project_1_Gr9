58fb019867efa7775715b38b663bfe4f11550f7f
sonbtph52402 <sonbtph52402@gmail.com>
2024-11-21 10:47:49 +0700

    them

--- commons/env.php
+++ commons/env.php
@@ -3,10 +3,10 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'   , 'http://localhost/Project-1/');
+define('BASE_URL'   , 'http://localhost/PROJECT_1_GR9/');
 
 define('DB_HOST'    , 'localhost');
-define('DB_PORT'    , 3306);
+define('DB_PORT'    , 3307);
 define('DB_NAME'    , 'duan1_gr9');  // Tên database
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
